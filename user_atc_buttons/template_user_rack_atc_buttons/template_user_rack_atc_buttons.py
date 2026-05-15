c98381825cd52f4741f1b8eed32c51f95a8ba349
S. Hallerbach <s.hallerbach@me.com>
2026-05-15 17:05:24 +0200

    Update to Debian Trixie

--- user_atc_buttons/template_user_rack_atc_buttons/template_user_rack_atc_buttons.py
+++ user_atc_buttons/template_user_rack_atc_buttons/template_user_rack_atc_buttons.py
@@ -1,12 +1,12 @@
 import os
 import linuxcnc
 
-from qtpy import uic
-from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QWidget
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget
 
 from qtpyvcp.plugins import getPlugin
 from qtpyvcp.utilities import logger
+import template_user_rack_atc_buttons_ui
 
 LOG = logger.getLogger(__name__)
 
@@ -19,5 +19,8 @@ INI_FILE = linuxcnc.ini(os.getenv('INI_FILE_NAME'))
 class UserRackAtcButton(QWidget):
     def __init__(self, parent=None):
         super(UserRackAtcButton, self).__init__(parent)
-        ui_file = os.path.splitext(os.path.basename(__file__))[0] + ".ui"
-        uic.loadUi(os.path.join(os.path.dirname(__file__), ui_file), self)
+        ui_cls = getattr(template_user_rack_atc_buttons_ui, "Ui_USER_RACK_ATC_BUTTONS", None)
+        if ui_cls is None:
+            ui_cls = template_user_rack_atc_buttons_ui.Ui_USER_ATC_BUTTONS
+        self.ui = ui_cls()
+        self.ui.setupUi(self)
